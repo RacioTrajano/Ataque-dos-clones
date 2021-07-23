d0e501a76ba3c8c2558df14b4ef2e7fc7f0ae2a7
racio <85686582+RacioTrajano@users.noreply.github.com>
2021-07-23 14:17:02 +0000

    melhorado

--- main.c
+++ main.c
@@ -1,33 +1,43 @@
 #include <stdio.h>
-#include<stdlib.h>
-
+#include <stdlib.h>
 
 int main () {
-  int tam_codigo;
-  int compressao;
-  int qtd_leituras=0;
 
-  int num_aparicao;
-  int valor;
-
-  int *N =&num_aparicao;
-  int *V =&valor;
-
-
-  scanf("%d %d", &tam_codigo, &compressao);
-
-  while (qtd_leituras != tam_codigo){
+int tam;
+int soma=0;
+int reducao=0;
+int N=0;
+int V=0;
+int *qtd = &N;
+int *valor = &V;
 
 
-    scanf("%d %d", N,V);
-    qtd_leituras= qtd_leituras + num_aparicao;
-    for(int i=0; i< num_aparicao - compressao;i++){
-      printf("%d ", valor);
-    }
+scanf("%d %d", &tam, &reducao);
 
+while(soma<tam){
+
+scanf("%d %d", qtd, valor);
+soma = soma + N;
+
+for (int i=0; i<reducao; i++)
+{
+  if(N ==1)
+  break;
+
+  else if (N %2 != 0){
+    N =N -1;
   }
 
+  N = N/2;
+}
+
+for (int i=0; i< N; i++){
+  printf("%d ", V);
+}
+}
 
 
+  
+  
   return 0;
 }
